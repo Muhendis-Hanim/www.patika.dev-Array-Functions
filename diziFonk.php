02a3d62dbe46fcd5f27485fb80025dfb3c14a7d1
Muhendis-Hanim <45447898+Muhendis-Hanim@users.noreply.github.com>
2023-01-17 00:21:03 +0300

    Add files via upload
    Write a function that creates an array from the specified number of random values by clearing the empty elements in the given array.

---
+++ diziFonk.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<pre>";
+/*
+$sayilar= range(1,20);  //range , 1 den 20ye kadar dizi oluşturur
+shuffle($sayilar);  //diziyi her seferınde karıtırır
+print_r($sayilar);
+*/
+/*
+$meyve= ['elma','armut','muz'];
+$harfler=['a','b','c'];
+$dizi= array_combine($meyve,$harfler);  //ıkı dızıyı alır ılkını key ıkıncıyı value yapar.sayılar eşit olmalı
+print_r($dizi);
+Array
+(
+    [elma] => a
+    [armut] => b
+[muz] => c
+)*/
+/*
+$meyve= ['elma','armut','muz','elma','cilek','muz','elma','armut','ananas','portakal','ananas','muz'];
+$dizi= array_count_values($meyve);      //aynı valuedan kac tane var onu sayar*/
+/*
+$harfler=['a','b','c','isim'=>'ahmet'];
+//$dizi=array_flip($harfler); //keyler value, valueler key olur yer degıstırırler
+$dizi= array_key_exists('isim',$harfler);   //isim keyi varmı bakar bool deger dondurur
+print_r($dizi);*/
+
